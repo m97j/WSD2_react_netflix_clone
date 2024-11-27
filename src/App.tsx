81ac5ee241bd063c96425f2a01c1cf20009f8999
m97j <mmnkjiae@gmail.com>
2024-11-27 16:07:14 +0900

    dir_set19(app.tsx)

---
+++ src/App.tsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import SignIn from './app/components/signIn/signin';
+import Home from './app/components/home/home_component';
+import HomeMain from './app/components/home/main/home-main_component';
+import HomePopular from './app/components/home/popular/home-popular_component';
+import HomeWishlist from './app/components/home/wishlist/home-wishlist_component';
+import HomeSearch from './app/components/search/home-search.component';
+import PrivateRoute from './app/guards/auth.guards'; // PrivateRoute 컴포넌트 임포트
+import './App.css';
+
+const App: React.FC = () => {
+  return (
+    <div className="app-root">
+      <Router>
+        <Routes>
+          <Route path="/" element={<Home />}>
+            <Route index element={<HomeMain />} />
+            <Route path="popular" element={<HomePopular />} />
+            <Route path="wishlist" element={<HomeWishlist />} />
+            <Route path="search" element={<HomeSearch />} />
+          </Route>
+          <Route path="/signin" element={<SignIn />} />
+        </Routes>
+      </Router>
+    </div>
+  );
+};
+
+export default App;
